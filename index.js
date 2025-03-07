d92dfd8fff9ae2fe6609ee06a06dc01e7522f243
hriday <alauddinhriday@gmail.com>
2025-03-07 16:26:49 +0600

    some changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: ["http://localhost:5173", "https://task-management-app-web.netlify.app"], // Replace with your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
